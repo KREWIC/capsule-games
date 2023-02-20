ad7c456280327fd617cd6e79114ba41bf075847d
Krewic <maxxpizzitola97@gmail.com>
2023-02-19 20:29:38 -0600

    Setup basic CSS for website

--- js/RPS.js
+++ js/RPS.js
@@ -3,7 +3,7 @@
         console.log(Math.floor((Math.random()*3))+1)
         
         let winCount = 0;
-        let lossCount =0;
+        let lossCount = 0;
         //^^ Track overall wins and losses
         
         let userChoice=0;
